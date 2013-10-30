8e49ba31cd968fce9ce48a15e06b344ca32a4774
Eric Rochester <erochest@virginia.edu>
2013-10-30 12:07:18 -0400

    Updated the XML to corpus script to process a directory.

--- bin/xml_to_corpus.py
+++ bin/xml_to_corpus.py
@@ -8,8 +8,8 @@ import lxml.etree as ET
 
 
 ## CHANGE THIS:
-INPUT_FILE = 'Classics/Plato/opensource/plat.tet3_eng.xml'
-OUTPUT_DIR = 'dialogs'
+INPUT_DIR = 'gk/Classics/Plato'
+OUTPUT_DIR = 'plato-text'
 ## Then, input the directory into mallet:
 ## > bin/mallet --input-dir dialogs
 ## See http://programminghistorian.org/lessons/topic-modeling-and-mallet
@@ -25,12 +25,11 @@ def get_text(el, buf):
             buf.append(child.tail)
 
 
-def main():
-    if not os.path.isdir(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
-
+def iter_file_text(filename):
+    """Return the text for a file."""
+    print('Parsing {}...'.format(filename))
     parser = ET.XMLParser(dtd_validation=True, no_network=False)
-    xml    = ET.parse(INPUT_FILE, parser)
+    xml    = ET.parse(filename, parser)
 
     n = 0
     for text in xml.findall('.//group/text'):
@@ -41,15 +40,33 @@ def main():
         get_text(text, buffer)
         text_content = ' '.join(buffer)
 
-        # make up a filename under OUTPUT_DIR
-        filename = os.path.join(OUTPUT_DIR, 'text-%04d.txt' % (n,))
+        yield text_content
+
+
+def save_file_texts(filename, output_prefix):
+    """Iterate over the file's texts and save to files with a given prefix."""
+    output_template = output_prefix + '-{:04d}.txt'
+    for (i, text) in enumerate(iter_file_text(filename)):
+        filename = output_template.format(i)
 
         # put the text content into the file
         with codecs.open(filename, 'w', encoding='utf8') as f:
-            f.write(text_content)
+            f.write(text)
 
         # print filename
         print(filename)
+
+
+def main():
+    if not os.path.isdir(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    output_base = os.path.join(OUTPUT_DIR, 'text')
+    for (root, dirs, files) in os.walk(INPUT_DIR):
+        for fn in files:
+            full_fn = os.path.join(root, fn)
+
+            save_file_texts(full_fn, output_base)
 
     print('done.')
 
